756f57afb3e6839b023e9d953001d7a3f5531d43
Phuoc Nguyen <vodanh.2901@gmail.com>
2020-08-28 00:58:59 +0700

    home screen

---
+++ lib/screens/info_card.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:phuoc_nguyen_covid/constants.dart';
+import 'package:phuoc_nguyen_covid/widgets/line_chart.dart';
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final int effectedNum;
+  final Color iconColor;
+  const InfoCard({
+    Key key,
+    this.title,
+    this.effectedNum,
+    this.iconColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          width: constraints.maxWidth / 2 - 10,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          color: iconColor.withOpacity(0.12),
+                          shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/icons/running.svg',
+                        height: 12,
+                        width: 12,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: RichText(
+                        text: TextSpan(
+                            style: TextStyle(color: kTextColor),
+                            children: [
+                              TextSpan(
+                                text: '$effectedNum \n',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .copyWith(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: 'People',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  height: 2,
+                                ),
+                              ),
+                            ]),
+                      ),
+                    ),
+                    Expanded(
+                      child: LineReportChart(),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
